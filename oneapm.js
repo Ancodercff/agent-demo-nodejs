c08ababc9b9cee2fb4d866ac5cf428624ea72662
Zhen Ju <juzhen@oneapm.com>
2015-11-06 10:40:19 +0800

    Add the sql trace items in config file.

--- oneapm.js
+++ oneapm.js
@@ -1,3 +1,15 @@
 exports.config = {
-    app_name: ['travisci_' + process.version]
+  app_name: ['travisci_' + process.version],
+  collect_traces : true,
+  slow_sql : {
+    enabled : true,
+    max_samples : 10
+  },
+  transaction_tracer : {
+    enabled : true,
+    transaction_threshold : 'apdex_f',
+    top_n : 20,
+    record_sql: 'raw',
+    explain_threshold : 500
+  }
 };
